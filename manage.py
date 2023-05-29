fea9a419b2095396b14d3056081faf26c6d856f0
coro1x00 <110147950+coro1x00@users.noreply.github.com>
2023-05-29 10:48:14 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ options = {
         'proxy': {
             #'http': 'socks5://ambon:gaming@143.198.198.4:2408', 
             #'https': 'socks5://ambon:gaming@143.198.198.4:2408',
-            'no_proxy': 'localhost,127.0.0.1'
+            #'no_proxy': 'localhost,127.0.0.1'
         }  
     }
 
@@ -20,4 +20,3 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome('chromedriver', options=chrome_options,seleniumwire_options=options)
 driver.get("https://shifu.mrpandabear.org/mine?wallet=002CA3573BA9FB3CEDB451A4D85E5A7D29DF5D2000DCC9AD27")
 print(driver.title)
-time.sleep(999999)
